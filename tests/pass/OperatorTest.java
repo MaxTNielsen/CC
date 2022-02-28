e1c4f2f893cb07fc1337ebc3cfb57b9d48627e92
simon <yconsj@gmail.com>
2022-02-28 19:21:00 +0100

    test operator

---
+++ tests/pass/OperatorTest.java
@@ -0,0 +1,11 @@
+public class OperatorTest {
+    public static void main(String[] args) {
+        int a = 25 << 1;
+        int a1 = 25 >> 1;
+        int a2 = 25 >>> 1;
+        a -=  5;
+        --a;
+        a <<= 5;
+        a2 = a ? 2 : 2;
+    }
+}
