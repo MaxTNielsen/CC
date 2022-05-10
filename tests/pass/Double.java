7ab07f363a9b413affd39b8c7ef93ae13cc2bc7f
simon <yconsj@gmail.com>
2022-05-10 10:08:49 +0200

    double test

--- tests/pass/Double.java
+++ tests/pass/Double.java
@@ -1,8 +1,36 @@
+package pass;
+import java.lang.System;
 public class Double {
     public static void main(String[] args) {
-        double x = 1.5;
-        double y = 2.5;
+        System.out.println(foo());
+    }
+    public static double foo(){
+        // HEY
+        /*  hey */
 
-        x = x*y;
+        /* *&//* 
+         **   heysda*
+        */
+        int i = 4;
+        double d= 0.30000;
+        double e = 5.33;
+
+        e = i*d;
+        return e;
+    }
+    static public double mul(double x, double y){
+        return x*y;
+    }
+    static public double div(double x, double y){
+        return x/y;
+    }
+    static public double plus(double x, double y){
+        return x+y;
+    }
+    static public double minus(double x, double y){
+        return x-y;
+    }
+    static public double tripple(double x, double y, double z){
+        return x+y+z;
     }
 }
